43e489ba4059d9abb408b158d6a796fd3d7654fd
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-29 14:28:44 +0200

    headerRight button moved from _layout to posts

--- app/(tabs)/posts.js
+++ app/(tabs)/posts.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { StyleSheet, Text, View, Button, Platform } from "react-native";
 
 export default function Posts() {
+    const router = useRouter();
+
+    function showCreateModal() {
+        router.push("/create");
+    }
     return (
         <View style={styles.container}>
+            <Stack.Screen
+                options={{
+                    headerRight: () => (
+                        <Button
+                            title="Add New"
+                            color={Platform.OS === "ios" ? "#fff" : "#264c59"}
+                            onPress={showCreateModal}
+                        />
+                    )
+                }}
+            />
             <View style={styles.main}>
                 <Text style={styles.title}>Posts</Text>
                 <Text style={styles.subtitle}>This is the first page of your app.</Text>
             </View>
         </View>
     );
+
+    <Button title="Close" color={Platform.OS === "ios" ? "#fff" : "#acc6c9"} onPress={() => router.back()} />;
 }
 
 const styles = StyleSheet.create({
