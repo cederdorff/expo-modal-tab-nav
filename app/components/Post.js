cb04dbab0dfb44d0cdd29613aa97c8a7585fe34f
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-30 12:38:47 +0200

    Update Post.js

--- app/components/Post.js
+++ app/components/Post.js
@@ -4,7 +4,7 @@ export default function PostListItem({ post }) {
     return (
         <View style={styles.postContainer}>
             <Image style={styles.postImage} source={{ uri: post.image }} />
-            <Text>{post.caption}</Text>
+            <Text style={styles.postCaption}>{post.caption}</Text>
         </View>
     );
 }
@@ -12,10 +12,14 @@ export default function PostListItem({ post }) {
 const styles = StyleSheet.create({
     postContainer: {
         flex: 1,
-        minHeight: 320
+        minHeight: 320,
+        paddingBottom: 40
     },
     postImage: {
         aspectRatio: 1,
         flex: 1
+    },
+    postCaption: {
+        fontSize: 35
     }
 });
