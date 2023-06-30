d0a2dcd9fd1356bd70dd91fe636ef98a4a793318
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-30 11:30:43 +0200

    Update posts.js

--- app/(tabs)/posts.js
+++ app/(tabs)/posts.js
@@ -1,8 +1,36 @@
 import { Stack, useRouter } from "expo-router";
-import { StyleSheet, Text, View, Button, Platform } from "react-native";
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
+import Post from "../components/Post";
 
 export default function Posts() {
     const router = useRouter();
+
+    const posts = [
+        {
+            caption: "Beautiful sunset at the beach",
+            createdAt: 1687215634430,
+            image: "https://images.unsplash.com/photo-1566241832378-917a0f30db2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            id: "-M1Abcdefg123"
+        },
+        {
+            caption: "Exploring the city streets of Aarhus",
+            createdAt: 1687615634430,
+            image: "https://images.unsplash.com/photo-1559070169-a3077159ee16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            id: "-M1Xyz987pqr"
+        },
+        {
+            caption: "Delicious food at the restaurant",
+            createdAt: 1687015634430,
+            image: "https://images.unsplash.com/photo-1548940740-204726a19be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            id: "-M2Def456stu"
+        },
+        {
+            caption: "Exploring the city center of Aarhus",
+            createdAt: 1687615704430,
+            image: "https://images.unsplash.com/photo-1612624629424-ddde915d3dc5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            uid: "-M3Ghi789vwx"
+        }
+    ];
 
     function showCreateModal() {
         router.push("/create");
@@ -23,11 +51,12 @@ export default function Posts() {
             <View style={styles.main}>
                 <Text style={styles.title}>Posts</Text>
                 <Text style={styles.subtitle}>This is the first page of your app.</Text>
+                {posts.map(post => (
+                    <Post post={post} />
+                ))}
             </View>
         </View>
     );
-
-    <Button title="Close" color={Platform.OS === "ios" ? "#fff" : "#acc6c9"} onPress={() => router.back()} />;
 }
 
 const styles = StyleSheet.create({
