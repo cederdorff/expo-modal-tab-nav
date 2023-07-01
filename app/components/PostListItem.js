6950e04604f40b873b1258e59f1150798a8b5afa
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-07-01 21:30:21 +0200

    Renamed

--- app/components/PostListItem.js
+++ app/components/PostListItem.js
@@ -13,13 +13,16 @@ const styles = StyleSheet.create({
     postContainer: {
         flex: 1,
         minHeight: 320,
-        paddingBottom: 40
+        paddingBottom: 30,
+        borderBottomColor: "#acc6c9",
+        borderBottomWidth: 0.5
     },
     postImage: {
         aspectRatio: 1,
         flex: 1
     },
     postCaption: {
-        fontSize: 35
+        fontSize: 22,
+        padding: 15
     }
 });
