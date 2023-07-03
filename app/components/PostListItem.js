59bb31d63cfccf6e4f5fa27a11d5ffc2f4ca80a0
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-07-03 09:05:27 +0200

    User avatar added

--- app/components/PostListItem.js
+++ app/components/PostListItem.js
@@ -1,4 +1,6 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Image, StyleSheet, Text, View } from "react-native";
+import Avatar from "./Avatar";
 
 export default function PostListItem({ post }) {
     function formatDate(timestamp) {
@@ -15,6 +17,10 @@ export default function PostListItem({ post }) {
 
     return (
         <View style={styles.postContainer}>
+            <View style={styles.headerContainer}>
+                <Avatar userId={post.uid} />
+                <Ionicons style={styles.moreBtn} name="ellipsis-horizontal" size={28} color="#264c59" />
+            </View>
             <Image style={styles.postImage} source={{ uri: post.image }} />
             <Text style={styles.postCaption}>{post.caption}</Text>
             <Text style={styles.postDate}>{formatDate(post.createdAt)}</Text>
@@ -41,5 +47,13 @@ const styles = StyleSheet.create({
     postDate: {
         fontSize: 15,
         paddingHorizontal: 15
+    },
+    headerContainer: {
+        alignItems: "center",
+        flexDirection: "row"
+    },
+    moreBtn: {
+        position: "absolute",
+        right: 10
     }
 });
